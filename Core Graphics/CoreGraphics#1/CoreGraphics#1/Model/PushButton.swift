ee3b8a9e69906d2a53e7d9dd53aecba1b562d123
Joao <joaovitornaru@hotmail.com>
2017-09-08 22:55:42 -0300

    MVC added

---
+++ Core Graphics/CoreGraphics#1/CoreGraphics#1/Model/PushButton.swift
@@ -0,0 +1,21 @@
+//
+//  PushButton.swift
+//  CoreGraphics#1
+//
+//  Created by Solid Jaum on 08/09/17.
+//  Copyright © 2017 Jaum. All rights reserved.
+//
+
+import UIKit
+
+class PushButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
